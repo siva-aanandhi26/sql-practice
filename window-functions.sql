47b06fa1e6391ab726aac3e94c2dd135e97edfaa
Siva Aanandhi <54903357+siva-aanandhi26@users.noreply.github.com>
2026-02-21 17:43:00 +0530

    Update window-functions.sql

--- window-functions.sql
+++ window-functions.sql
@@ -1,6 +1,7 @@
   ----WINDOW FUNCTIONS----
 
-Write a query to find the top two highest grossing products with each category in the year 2026. The output should include the category, product and total spend.
+BASIC RANKING WITHOUT TIMESTAMP WALA!!!
+1. Write a query to find the top two highest grossing products with each category in the year 2026. The output should include the category, product and total spend.
  PRODUCTS: 
 category | product | user_id | spend | transaction_date | transaction_time
 
@@ -16,5 +17,9 @@ with rank_spend as(
 )
 
 select category,product,total_spend from rank_spend where rank<=2 order by category,rank;
+
+
+RUNNING TOTAL LOGIC WITH WINDOWS
+2. select sales_date, sales_amount, SUM(sales_amount) OVER(ORDER by sales_date) as running_total from sales;
 
 
